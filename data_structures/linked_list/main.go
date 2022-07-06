598c70b1b4676d959693fbabb9320c33fa3914bb
damurka5 <damurka777@gmail.com>
2022-07-06 19:43:19 +0300

    linked list

--- data_structures/linked_list/main.go
+++ data_structures/linked_list/main.go
@@ -5,16 +5,51 @@ import "fmt"
 type Node struct {
 	value int
 	next  *Node
+	prev  *Node
 }
 
-func main() {
-	node1 := Node{3, nil}
-	node2 := Node{5, &node1}
-	node3 := Node{7, &node2}
-	node4 := Node{10, &node3}
-	pointer := &node4
+type LinkedList struct {
+	head *Node // the first element od the LinkedList
+	len  int
+}
+
+// adds new node to the LinkedList as a head
+func (l *LinkedList) addNode(value int) {
+	new_node := Node{
+		value: value,
+		next:  nil,
+	}
+	if l.len == 0 {
+		l.head = &new_node
+		l.len++
+		return
+	}
+
+	pointer := l.head
+	for i := 0; i < l.len; i++ {
+		if pointer.next == nil {
+			pointer.next = &new_node
+			new_node.prev = pointer
+			l.len++
+			return
+		}
+		pointer = pointer.next
+	}
+}
+
+func (l LinkedList) printList() {
+	pointer := l.head
 	for pointer != nil {
 		fmt.Println(pointer.value)
 		pointer = pointer.next
 	}
+}
+
+func main() {
+	link := LinkedList{}
+	link.addNode(1)
+	link.addNode(2)
+	link.addNode(3)
+	link.addNode(4)
+	link.printList()
 }
