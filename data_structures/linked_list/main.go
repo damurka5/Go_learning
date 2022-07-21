0f2a875645af20d3d8ac17819bfa20414d55c104
damurka5 <damurka777@gmail.com>
2022-07-21 16:05:47 +0300

    more functions

--- data_structures/linked_list/main.go
+++ data_structures/linked_list/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Node struct {
 	value int
@@ -15,12 +17,12 @@ type LinkedList struct {
 
 // adds new node to the LinkedList as a head
 func (l *LinkedList) addNode(value int) {
-	new_node := Node{
+	newNode := Node{
 		value: value,
 		next:  nil,
 	}
 	if l.len == 0 {
-		l.head = &new_node
+		l.head = &newNode
 		l.len++
 		return
 	}
@@ -28,10 +30,50 @@ func (l *LinkedList) addNode(value int) {
 	pointer := l.head
 	for i := 0; i < l.len; i++ {
 		if pointer.next == nil {
-			pointer.next = &new_node
-			new_node.prev = pointer
+			pointer.next = &newNode
+			newNode.prev = pointer
 			l.len++
 			return
+		}
+		pointer = pointer.next
+	}
+}
+
+// deletes the first node with a given value
+func (l *LinkedList) deleteNode(value int) {
+	pointer := l.head
+	for pointer != nil {
+		if pointer.value == value {
+			if pointer.prev == nil {
+				l.head = pointer.next
+			} else {
+				temp := pointer.prev
+				temp.next = pointer.next
+			}
+			pointer = nil
+			break
+		}
+		pointer = pointer.next
+	}
+}
+
+// inserts new node after given node (first met value)
+func (l *LinkedList) insertAfter(value int, newValue int) {
+	if l.len == 0 { // this is an empty list
+		return
+	}
+
+	newNode := Node{
+		value: newValue,
+	}
+
+	pointer := l.head
+	for pointer != nil {
+		if pointer.value == value {
+			newNode.next = pointer.next
+			pointer.next = &newNode
+			newNode.prev = pointer
+			break
 		}
 		pointer = pointer.next
 	}
@@ -51,5 +93,7 @@ func main() {
 	link.addNode(2)
 	link.addNode(3)
 	link.addNode(4)
+	link.printList()
+	link.insertAfter(2, 35)
 	link.printList()
 }
